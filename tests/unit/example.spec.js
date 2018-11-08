0450c9411dd2309f6529d37b40f86debbfc62db5
Alexandr Promakh <s-promakh@ya.ru>
2018-11-08 12:39:15 +0500

    Update example.spec.js

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -5,7 +5,7 @@ describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      propsData: { msg: "123" }
     })
     expect(wrapper.text()).toMatch(msg)
   })
